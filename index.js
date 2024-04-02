9ee9f8a36a8fdc55d948c7c58f1077a0ec211434
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-04-02 12:08:39 +0300

    work

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write youcr code here!
+const mainElement = document.querySelector(`main#main`)
+mainElement.remove();
+const newHeader = document.createElement(`h1`)
+newHeader.setAttribute(`id`,`victory`);
+newHeader.innerHTML = `Your Name is the champion`
